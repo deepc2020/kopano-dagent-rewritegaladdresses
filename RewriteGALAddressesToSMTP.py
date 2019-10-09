25acbc16eb29bc3d28a2904420754683164f1c21
deepc2020 <56347351+deepc2020@users.noreply.github.com>
2019-10-09 13:07:46 +0200

    Updates for Python 3
    - unicode -> str
    - str -> bytes
    - headers is no string anymore

--- RewriteGALAddressesToSMTP.py
+++ RewriteGALAddressesToSMTP.py
@@ -20,13 +20,13 @@ class RewriteGALAddressesToSMTP(IMapiDAgentPlugin):
         decodedHeader = decode_header(header)
         headerText, headerEncoding = decodedHeader[0]
         if headerEncoding is None:
-            return unicode(headerText)
+            return str(headerText)
         else:
-            return unicode(headerText, headerEncoding, 'ignore')
+            return str(headerText, headerEncoding, 'ignore')
 
     def PreDelivery(self, session, addrbook, store, folder, message):
         headers = message.GetProps([PR_TRANSPORT_MESSAGE_HEADERS], 0)[0].Value
-        msg = email.message_from_string(headers)
+        msg = email.message_from_bytes(headers)
         to_addrs = getaddresses(msg.get_all('to', []))
         cc_addrs = getaddresses(msg.get_all('cc', []))
     
@@ -36,7 +36,7 @@ class RewriteGALAddressesToSMTP(IMapiDAgentPlugin):
                 SPropValue(PR_RECIPIENT_TYPE, MAPI_TO),
                 SPropValue(PR_DISPLAY_NAME_W, self.DecodeHeaderToUTF8(addr[0])),
                 SPropValue(PR_ADDRTYPE, 'SMTP'),
-                SPropValue(PR_EMAIL_ADDRESS, unicode(addr[1])),
+                SPropValue(PR_EMAIL_ADDRESS, bytes(addr[1],'utf-8')),
             ])
     
         for addr in cc_addrs:
@@ -44,7 +44,7 @@ class RewriteGALAddressesToSMTP(IMapiDAgentPlugin):
                 SPropValue(PR_RECIPIENT_TYPE, MAPI_CC),
                 SPropValue(PR_DISPLAY_NAME_W, self.DecodeHeaderToUTF8(addr[0])),
                 SPropValue(PR_ADDRTYPE, 'SMTP'),
-                SPropValue(PR_EMAIL_ADDRESS, unicode(addr[1])),
+                SPropValue(PR_EMAIL_ADDRESS, bytes(addr[1],'utf-8')),
             ])
     
         message.ModifyRecipients(0, names)
